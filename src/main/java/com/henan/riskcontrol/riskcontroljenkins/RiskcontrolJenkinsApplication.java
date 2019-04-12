4f1c3cf67cd41e1a86c56ce0c83322224fceb83c
chenyuqg <chenyuqg>
2019-04-12 23:18:15 +0800

    init commit;

---
+++ src/main/java/com/henan/riskcontrol/riskcontroljenkins/RiskcontrolJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.henan.riskcontrol.riskcontroljenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskcontrolJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskcontrolJenkinsApplication.class, args);
+    }
+
+}
---
+++ test.sh
@@ -0,0 +1,8 @@
+gradle assemble
+gradle compileJava
+gradle compileScala
+gradle processResources
+gradle classes
+gradle jar
+gradle sourcesJar
+gradle assemble
